f16fe98c254fe4acd1865331cfbc295b2c117426
Taichi Kato <me@taichikato.com>
2019-01-25 23:58:45 +0800

    circle of life

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,6 +4,7 @@ vector<Line> lines;
 vector<Line> immutableLines;
 vector<Line> nLines;
 int generation = 1;
+int frame = 0;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
@@ -11,13 +12,34 @@ void ofApp::setup(){
     ofEnableAntiAliasing();
     if(lines.size() == 0){
         Line line = Line(ofGetWindowWidth()/4, ofGetWindowHeight(), ofGetWindowWidth()/4, 100, 32);;
+        Line line2 = Line(ofGetWindowWidth()*3/4, ofGetWindowHeight(), ofGetWindowWidth()*3/4, 200, 32);;
         lines.push_back(line);
+        lines.push_back(line2);
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    if(frame > 240 && generation < 10){
+        nLines.clear();
+        for(int i = 0; i < lines.size(); i++){
+            vector<Line> tempLines = lines[i].generate(PI/4, generation);
+            immutableLines.push_back(tempLines.front());
+            tempLines.erase(tempLines.begin());
+            nLines.insert(nLines.end(), tempLines.begin(), tempLines.end());
+        }
+        lines = nLines;
+        generation = generation+1;
+        frame = 0;
+    }else if(generation >= 10){
+        lines.clear();
+        immutableLines.clear();
+        Line line = Line(ofGetWindowWidth()/4, ofGetWindowHeight(), ofGetWindowWidth()/4, 100, 32);;
+        lines.push_back(line);
+        generation = 0;
+    }else{
+        frame++;
+    }
 }
 
 //--------------------------------------------------------------
@@ -36,7 +58,9 @@ void ofApp::keyPressed(int key){
         lines.clear();
         immutableLines.clear();
         Line line = Line(ofGetWindowWidth()/4, ofGetWindowHeight(), ofGetWindowWidth()/4, 100, 32);;
+        Line line2 = Line(ofGetWindowWidth()*3/4, ofGetWindowHeight(), ofGetWindowWidth()*3/4, 200, 32);;
         lines.push_back(line);
+        lines.push_back(line2);
     }
 }
 
